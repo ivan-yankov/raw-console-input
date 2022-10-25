2718d2e0c0a51db457c241d14a97661cf733ee36
Ivan <ivan.iankov@gmail.com>
2022-10-25 16:53:05 +0300

    Improvements

--- src/main/java/console/ConsoleReader.java
+++ src/main/java/console/ConsoleReader.java
@@ -2,9 +2,23 @@ package console;
 
 public class ConsoleReader {
     public static Key readKey() {
+        return readKey(false);
+    }
+
+    public static Key readKey(boolean printCode) {
         try {
             int[] code = RawConsoleInput.read(true);
-            return KeyAnalyzer.analyzeKey(code);
+
+            if (printCode) {
+                System.out.println();
+                System.out.println("Raw code:");
+
+                for (int i : code) {
+                    System.out.println(i);
+                }
+            }
+
+                return KeyAnalyzer.analyzeKey(code);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             return new Key("Unknown");
--- src/main/java/console/Main.java
+++ src/main/java/console/Main.java
@@ -3,7 +3,7 @@ package console;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Press a key...");
-        Key k = ConsoleReader.readKey();
+        Key k = ConsoleReader.readKey(true);
 
         System.out.println();
         System.out.println("Key: " + k.getName());
